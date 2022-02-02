10f38ef8a50ff7a47b2c0d0a251f5b4d32194cc3
Edwin Codrington <edwincodrington@gmail.com>
2022-02-01 19:03:33 -0500

    01 and 02 completed

--- src/00-numbers.js
+++ src/00-numbers.js
@@ -4,7 +4,13 @@
  * @param {number[]} values - An array of whole numbers.
  * @returns {number} - The sum of all numbers in the array.
  */
-function addAllNumbers(values) {}
+function addAllNumbers(values) {
+  let sum=0;
+  for(let num of values){
+    sum+= num;
+  }
+  return sum;
+}
 
 /**
  * Multiply each number in the array by 2 and return a new array with the doubled values.
@@ -12,7 +18,13 @@ function addAllNumbers(values) {}
  * @param {number[]} values - An array of whole numbers.
  * @returns {number[]} - An array of whole numbers.
  */
-function doubleAllNumbers(values) {}
+function doubleAllNumbers(values) {
+  let arr=[];
+  for(let num of values){
+    arr.push(num*2);
+  }
+  return arr;
+}
 
 /**
  * Return an array of only the even numbers in the inputted array.
@@ -20,7 +32,15 @@ function doubleAllNumbers(values) {}
  * @param {number[]} values - An array of whole numbers.
  * @returns {number[]} - An array of only even numbers.
  */
-function getAllEvenNumbers(values) {}
+function getAllEvenNumbers(values) {
+  let arr=[];
+  for(let num of values){
+    if(!(num%2)){
+      arr.push(num);
+    }
+  }
+  return arr;
+}
 
 /**
  * Return the number given if it is in the array. Otherwise, return `null`.
@@ -29,7 +49,15 @@ function getAllEvenNumbers(values) {}
  * @param {number} val - A specific value to find.
  * @returns {number|null} - The found number or `null`.
  */
-function findNumberByValue(values, val) {}
+function findNumberByValue(values, val) {
+  let found= null;
+  for(let num of values){
+    if(num === val){
+      found= num;
+    }
+  }
+  return found;
+}
 
 /**
  * Return `true` if all numbers in the array are negative. Otherwise, return `false`. Ignore `0` in determining whether or not an array includes all negative numbers.
@@ -37,7 +65,25 @@ function findNumberByValue(values, val) {}
  * @param {number[]} values - An array of whole numbers.
  * @returns {boolean} - Whether or not all numbers in the array are negative.
  */
-function allNumbersAreNegative(values) {}
+function allNumbersAreNegative(values) {
+  if(!values.length){
+    return false;
+  }
+  let negative = false;
+  for(let num of values){
+    //num = Number(num);
+    if(num < 0){
+      negative = true;
+      //return negative;
+    } else if(num === 0){
+      continue;
+    } else {
+      negative = false;
+      return negative;
+    }
+  }
+  return negative;
+}
 
 /**
  * Return the largest number in the array. If the array is empty, return `null`.
@@ -45,7 +91,19 @@ function allNumbersAreNegative(values) {}
  * @param {number[]} values - An array of whole numbers.
  * @returns {number} - The largest number in the array.
  */
-function getLargestNumber(values) {}
+function getLargestNumber(values) {
+  if(!values.length){
+    return null;
+  }
+  let largest= values[0];
+  for(let val of values){
+    if(val > largest){
+      largest=val;
+    }
+  }
+  return largest;
+}
+
 
 module.exports = {
   addAllNumbers,
