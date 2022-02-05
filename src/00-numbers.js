c633a575d45645f6c0a2b989bca20ac3f79d6f5d
Jorge Carrera <JCarrera994@gmail.com>
2022-02-05 15:06:25 -0500

    fixes typo in numbers.test

--- src/00-numbers.js
+++ src/00-numbers.js
@@ -4,7 +4,15 @@
  * @param {number[]} values - An array of whole numbers.
  * @returns {number} - The sum of all numbers in the array.
  */
-function addAllNumbers(values) {}
+function addAllNumbers(values) {
+  let sum = 0;
+
+  for (let i = 0; i < values.length; i++){
+    sum += values[i]
+  }
+
+  return sum
+}
 
 /**
  * Multiply each number in the array by 2 and return a new array with the doubled values.
@@ -12,7 +20,15 @@ function addAllNumbers(values) {}
  * @param {number[]} values - An array of whole numbers.
  * @returns {number[]} - An array of whole numbers.
  */
-function doubleAllNumbers(values) {}
+function doubleAllNumbers(values) {
+  let doubledNums = []
+
+  for (let i = 0; i < values.length; i++){
+    doubledNums.push(values[i] * 2)
+  }
+
+  return doubledNums
+}
 
 /**
  * Return an array of only the even numbers in the inputted array.
@@ -20,7 +36,17 @@ function doubleAllNumbers(values) {}
  * @param {number[]} values - An array of whole numbers.
  * @returns {number[]} - An array of only even numbers.
  */
-function getAllEvenNumbers(values) {}
+function getAllEvenNumbers(values) {
+  let evenNums = []
+
+  for (let i = 0; i < values.length; i++){
+    if (values[i] % 2 === 0){
+      evenNums.push(values[i])
+    }
+  }
+
+  return evenNums
+}
 
 /**
  * Return the number given if it is in the array. Otherwise, return `null`.
@@ -29,7 +55,17 @@ function getAllEvenNumbers(values) {}
  * @param {number} val - A specific value to find.
  * @returns {number|null} - The found number or `null`.
  */
-function findNumberByValue(values, val) {}
+function findNumberByValue(values, val) {
+  let foundNum = null;
+
+  for (let i = 0; i < values.length; i++){
+    if (values[i] === val){
+      foundNum = val
+    }
+  }
+
+  return foundNum
+}
 
 /**
  * Return `true` if all numbers in the array are negative. Otherwise, return `false`. Ignore `0` in determining whether or not an array includes all negative numbers.
@@ -37,7 +73,9 @@ function findNumberByValue(values, val) {}
  * @param {number[]} values - An array of whole numbers.
  * @returns {boolean} - Whether or not all numbers in the array are negative.
  */
-function allNumbersAreNegative(values) {}
+function allNumbersAreNegative(values) {
+  for (let i = 0; i)
+}
 
 /**
  * Return the largest number in the array. If the array is empty, return `null`.
