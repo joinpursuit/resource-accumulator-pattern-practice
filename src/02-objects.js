0a39d24e5312518ecf4661710ea73250e2915d53
ziqian pan <ziqianpan1998@gmail.com>
2022-02-05 15:45:50 -0500

    finished 01 and 02

--- src/02-objects.js
+++ src/02-objects.js
@@ -4,7 +4,15 @@
  * @param {Object[]} people - An array of people objects.
  * @returns {string[]} - An array of names.
  */
-function getAllNames(people) {}
+function getAllNames(people) {
+let result = [];
+
+for (let i = 0; i < people.length; i++){
+  result.push(people[i].name);
+
+}
+return result;
+}
 
 /**
  * Given an array of people objects, return the single object with a name that matches the given name (case-insensitive). If no name matches, return `null`
@@ -13,7 +21,18 @@ function getAllNames(people) {}
  * @param {string} name - A single name.
  * @returns {Object|null} - A person object or `null`.
  */
-function findPersonByName(people, name) {}
+function findPersonByName(people, name) {
+let result = {};
+
+for (let i = 0; i < people.length; i++){
+  if (people[i].name.toUpperCase() === name.toUpperCase()){
+    result = people[i];// first time having to return the whole object. 
+  } else {
+    result = null;
+  }
+}
+return result;
+}
 
 /**
  * Return an array of objects, where each object represents a person under the age of 25.
@@ -21,7 +40,16 @@ function findPersonByName(people, name) {}
  * @param {Object[]} people - An array of people objects.
  * @returns {Object[]} - An array of people.
  */
-function getPeopleUnder25(people) {}
+function getPeopleUnder25(people) {
+let result = [];
+for (let i = 0; i < people.length; i++){
+  if (people[i].age < 25){
+    result.push(people[i]);
+  }
+}
+
+return result;
+}
 
 /**
  * Return an array of objects, where each object represents a person with a `gmail.com` email address.
@@ -29,7 +57,17 @@ function getPeopleUnder25(people) {}
  * @param {Object[]} people - An array of people objects.
  * @returns {Object[]} - An array of people.
  */
-function getPeopleWithGmail(people) {}
+function getPeopleWithGmail(people) {
+let result  = [];
+
+for (let i = 0; i < people.length; i++){
+  if (people[i].email.includes("gmail.com")){
+    result.push(people[i]);
+  }  
+}
+
+return result; 
+}
 
 module.exports = {
   getAllNames,
